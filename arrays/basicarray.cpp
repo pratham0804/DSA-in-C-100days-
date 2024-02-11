40cc7981c23817a47a5620d4babd103735610386
pratham0804 <149933759+pratham0804@users.noreply.github.com>
2024-02-11 12:52:47 +0000

    .

---
+++ arrays/basicarray.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+// define array globally , all the entries are zero.
+// array of maximum 10 to the power of 7 can be allocated outside main
+
+int main(){
+// array defined inside main , has the garbage values
+// array of maximum 10 to the power of 6 can be allocated in main 
+
+    return 0;
+}
---
+++ arrays/largest_element.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    return 0;
+}
+
+
+// first think about the  brute force solution , then seek for the better solution  
+// and then best solution.
+
+// brute solution : 
